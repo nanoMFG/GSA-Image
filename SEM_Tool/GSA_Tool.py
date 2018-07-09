1750697be60a0584164cc832d9ab0397743053c3
Joshua Schiller <jaschil2@illinois.edu>
2018-07-09 16:03:14 -0500

    Added more functionality. Contour finding still having trouble

--- SEM_Tool/GSA_Tool.py
+++ SEM_Tool/GSA_Tool.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-from PyQt5 import QtGui
+from PyQt5 import QtGui, QtCore
 import pyqtgraph as pg
 
 class ImageAnalyzer:
@@ -16,13 +16,22 @@ class ImageAnalyzer:
     self.wOpenFileBtn = QtGui.QPushButton('Open Image')
     self.wOpenFileBtn.clicked.connect(self.openFile)    
 
-    self.mod_dict = {'Color Mask': ColorMask}    
+    self.mod_dict = {
+    'Color Mask': ColorMask,
+    'Canny Edge Detector': CannyEdgeDetection,
+    'Dilate': Dilation,
+    'Erode': Erosion,
+    'Binary Mask': BinaryMask,
+    'Find Contours': FindContours
+    }    
     
     self.wComboBox = pg.ComboBox()
     self.wComboBox.addItem('Color Mask')
     self.wComboBox.addItem('Binary Mask')
-    self.wComboBox.addItem('Denoise')
+    # self.wComboBox.addItem('Denoise')
     self.wComboBox.addItem('Canny Edge Detector')
+    self.wComboBox.addItem('Dilate')
+    self.wComboBox.addItem('Erode')
     self.wComboBox.addItem('Find Contours')
 
     self.wAddMod = QtGui.QPushButton('Add Modification')
@@ -34,6 +43,7 @@ class ImageAnalyzer:
     self.wModList = QtGui.QListWidget()
     self.wModList.setSelectionMode(QtGui.QAbstractItemView.SingleSelection)
     self.wModList.itemClicked.connect(self.selectMod)
+    self.wModList.itemSelectionChanged.connect(self.selectMod)
     
     self.wImgBox = pg.GraphicsLayoutWidget()
     self.wImgBox_VB = self.wImgBox.addViewBox(row=1,col=1)
@@ -80,7 +90,10 @@ class ImageAnalyzer:
       selection = selection[0]
       mod = self.modifications.back_traverse(self.wModList.count() - self.wModList.row(selection) - 1)
       mod.update_image()
-      self.updateModWidget(mod.widget())
+      if self.wModList.row(selection) < self.wModList.count() - 1:
+        self.updateModWidget(None)
+      else:
+        self.updateModWidget(mod.widget())
 
   def removeMod(self):
     selection = self.wModList.selectedItems()
@@ -98,10 +111,10 @@ class ImageAnalyzer:
       pass
     elif self.modifications != None:
       mod = self.mod_dict[value](self.modifications,self.wImgItem)
+      self.modifications = mod
       widget = mod.widget()
       self.layout.addWidget(widget,6,1)
       self.updateAll(mod)
-      self.modifications = mod
     
   def openFile(self):
     self.img_file_path = QtGui.QFileDialog.getOpenFileName()
@@ -115,20 +128,10 @@ class ImageAnalyzer:
     
     mod = InitialImage(img_item=self.wImgItem)
     mod.set_image(self.img_data)
-
-    self.updateAll(mod)
     self.modifications = mod
-
+    
+    self.updateAll(mod)
     self.w.setWindowTitle(self.img_fname)
-  
-  # def cannyEdgeDetection(self,low_thresh=None,high_thresh=None):
-  #   if not isinstance(low_thresh,float):
-  #     low_thresh = max(self.img_data.flatten())*.1
-  #   if not isinstance(high_thresh,float):
-  #     high_thresh = max(self.img_data.flatten())*.4
-  #   self.img_state = cv2.GaussianBlur(self.img_state,(5,5),0)
-  #   self.img_state = cv2.Canny(self.img_state,low_thresh,high_thresh)
-  #   self.wImgItem.setImage(self.img_state,levels=(0,255))
   
   def run(self):
     self.w.show()
@@ -216,14 +219,225 @@ class ColorMask(Modification):
 class CannyEdgeDetection(Modification):
   def __init__(self,mod_in,img_item):
     super(CannyEdgeDetection,self).__init__(mod_in,img_item)
-    self.wToolBox = pg.GraphicsLayoutWidget()
+    
+    self.low_thresh = int(max(self.mod_in.image().flatten())*.1)
+    self.high_thresh = int(max(self.mod_in.image().flatten())*.4)
+    self.gauss_size = 5
+    self.wToolBox = pg.LayoutWidget()
+
+    self.wGaussEdit = QtGui.QLineEdit(str(self.gauss_size))
+    self.wGaussEdit.setValidator(QtGui.QIntValidator(3,51))
+    self.wGaussEdit.setFixedWidth(60)
+
+    self.wLowSlider = QtGui.QSlider(QtCore.Qt.Horizontal)
+    self.wLowSlider.setMinimum(0)
+    self.wLowSlider.setMaximum(255)
+    self.wLowSlider.setSliderPosition(int(self.low_thresh))
+    self.wLowEdit = QtGui.QLineEdit(str(self.low_thresh))
+    self.wLowEdit.setFixedWidth(60)
+    self.wLowEdit.setValidator(QtGui.QIntValidator(0,255))
+
+    self.wHighSlider = QtGui.QSlider(QtCore.Qt.Horizontal)
+    self.wHighSlider.setMinimum(0)
+    self.wHighSlider.setMaximum(255)
+    self.wHighSlider.setSliderPosition(int(self.high_thresh))
+    self.wHighEdit = QtGui.QLineEdit(str(self.high_thresh))
+    self.wHighEdit.setFixedWidth(60)
+    self.wHighEdit.setValidator(QtGui.QIntValidator(0,255))
+
+    self.wGaussEdit.returnPressed.connect(self._update_sliders)
+    self.wLowSlider.sliderReleased.connect(self._update_texts)
+    self.wLowSlider.sliderMoved.connect(self._update_texts)
+    self.wLowEdit.returnPressed.connect(self._update_sliders)
+    self.wHighSlider.sliderReleased.connect(self._update_texts)
+    self.wHighSlider.sliderMoved.connect(self._update_texts)
+    self.wHighEdit.returnPressed.connect(self._update_sliders)
+
+    self.wToolBox.addWidget(QtGui.QLabel('Gaussian Size'),0,0)
+    self.wToolBox.addWidget(QtGui.QLabel('Low Threshold'),1,0)
+    self.wToolBox.addWidget(QtGui.QLabel('High Threshold'),2,0)
+    self.wToolBox.addWidget(self.wGaussEdit,0,1)
+    self.wToolBox.addWidget(self.wLowEdit,1,1)
+    self.wToolBox.addWidget(self.wHighEdit,2,1)
+    self.wToolBox.addWidget(self.wLowSlider,1,2)
+    self.wToolBox.addWidget(self.wHighSlider,2,2)
+
   def name(self):
     return 'Canny Edge Detection'
-  def update_image(self):
-    pass
-  def widget(self):
-    pass
 
+  def _update_sliders(self):
+    self.gauss_size = int(self.wGaussEdit.text())
+    self.gauss_size = self.gauss_size + 1 if self.gauss_size % 2 == 0 else self.gauss_size
+    self.wGaussEdit.setText(str(self.gauss_size))
+    self.low_thresh = int(self.wLowEdit.text())
+    self.high_thresh = int(self.wHighEdit.text())
+
+    self.wHighSlider.setSliderPosition(self.low_thresh)    
+    self.wHighSlider.setSliderPosition(self.high_thresh)
+
+    self.update_image()
+
+  def _update_texts(self):
+    self.low_thresh = int(self.wLowSlider.value())
+    self.high_thresh = int(self.wHighSlider.value())
+
+    self.wLowEdit.setText(str(self.low_thresh))
+    self.wHighEdit.setText(str(self.high_thresh))
+
+    self.update_image()
+
+  def update_image(self):
+    self.img_out = cv2.GaussianBlur(self.mod_in.image(),(self.gauss_size,self.gauss_size),0)
+    self.img_out = 255-cv2.Canny(self.img_out,self.low_thresh,self.high_thresh)
+    self.img_item.setImage(self.img_out,levels=(0,255))
+  
+  def widget(self):
+    return self.wToolBox
+
+class Dilation(Modification):
+  def __init__(self,mod_in,img_item):
+    super(Dilation,self).__init__(mod_in,img_item)
+    self.size = 1
+    self.wToolBox = pg.LayoutWidget()
+    self.wSizeEdit = QtGui.QLineEdit(str(self.size))
+    self.wSizeEdit.setValidator(QtGui.QIntValidator(1,20))
+    self.wSizeEdit.setFixedWidth(60)
+    self.wSizeSlider = QtGui.QSlider(QtCore.Qt.Horizontal)
+    self.wSizeSlider.setMinimum(1)
+    self.wSizeSlider.setMaximum(20)
+    self.wSizeSlider.setSliderPosition(int(self.size))
+
+    # self.wSizeSlider.sliderReleased.connect(self._update_texts)
+    self.wSizeSlider.valueChanged.connect(self._update_texts)
+    self.wSizeEdit.returnPressed.connect(self._update_sliders)
+
+    self.wToolBox.addWidget(QtGui.QLabel('Kernel Size'),0,0)
+    self.wToolBox.addWidget(self.wSizeEdit,0,1)
+    self.wToolBox.addWidget(self.wSizeSlider,0,2)
+
+  def name(self):
+    return 'Dilation'
+
+  def _update_sliders(self):
+    self.size = int(self.wSizeEdit.text())
+    self.wSizeSlider.setSliderPosition(self.size)
+    self.update_image()
+
+  def _update_texts(self):
+    self.size = int(self.wSizeSlider.value())
+    self.wSizeEdit.setText(str(self.size))
+    self.update_image()
+
+  def update_image(self):
+    self.img_out = cv2.erode(self.mod_in.image().copy(),np.ones((self.size,self.size),np.uint8),iterations=1)
+    self.img_item.setImage(self.img_out,levels=(0,255))
+
+  def widget(self):
+    return self.wToolBox
+
+class Erosion(Modification):
+  def __init__(self,mod_in,img_item):
+    super(Erosion,self).__init__(mod_in,img_item)
+    self.size = 1
+    self.wToolBox = pg.LayoutWidget()
+    self.wSizeEdit = QtGui.QLineEdit(str(self.size))
+    self.wSizeEdit.setValidator(QtGui.QIntValidator(1,20))
+    self.wSizeEdit.setFixedWidth(60)
+    self.wSizeSlider = QtGui.QSlider(QtCore.Qt.Horizontal)
+    self.wSizeSlider.setMinimum(1)
+    self.wSizeSlider.setMaximum(20)
+    self.wSizeSlider.setSliderPosition(int(self.size))
+
+    # self.wSizeSlider.sliderReleased.connect(self._update_texts)
+    self.wSizeSlider.valueChanged.connect(self._update_texts)
+    self.wSizeEdit.returnPressed.connect(self._update_sliders)
+
+    self.wToolBox.addWidget(QtGui.QLabel('Kernel Size'),0,0)
+    self.wToolBox.addWidget(self.wSizeEdit,0,1)
+    self.wToolBox.addWidget(self.wSizeSlider,0,2)
+
+  def name(self):
+    return 'Erosion'
+
+  def _update_sliders(self):
+    self.size = int(self.wSizeEdit.text())
+    self.wSizeSlider.setSliderPosition(self.size)
+    self.update_image()
+
+  def _update_texts(self):
+    self.size = int(self.wSizeSlider.value())
+    self.wSizeEdit.setText(str(self.size))
+    self.update_image()
+
+  def update_image(self):
+    self.img_out = cv2.dilate(self.mod_in.image(),np.ones((self.size,self.size),np.uint8),iterations=1)
+    self.img_item.setImage(self.img_out,levels=(0,255))
+
+  def widget(self):
+    return self.wToolBox
+
+class BinaryMask(Modification):
+  def __init__(self,mod_in,img_item):
+    super(BinaryMask,self).__init__(mod_in,img_item)
+
+  def name(self):
+    return 'Binary Mask'
+
+  def update_image(self):
+    self.img_out = self.mod_in.image()
+    self.img_out[self.img_out < 255] = 0
+    self.img_item.setImage(self.img_out,levels=(0,255))
+
+  def widget(self):
+    return None
+
+class FindContours(Modification):
+  def __init__(self,mod_in,img_item):
+    super(FindContours,self).__init__(mod_in,img_item)
+    self.wLayout = pg.LayoutWidget()
+
+    self._img, self.contours, self.hierarchy = cv2.findContours(
+      self.mod_in.image().copy(),
+      cv2.RETR_LIST,
+      cv2.CHAIN_APPROX_SIMPLE)
+
+    self.contour_dict = {}
+    
+    self.wContourList = QtGui.QListWidget()
+    self.wContourList.setSelectionMode(QtGui.QAbstractItemView.SingleSelection)
+    for i,cnt in enumerate(self.contours):
+      key = '%d Contour'%i
+      self.contour_dict[key] = {}
+      self.contour_dict[key]['index'] = i
+      self.contour_dict[key]['list_item'] = QtGui.QListWidgetItem(key)
+      self.contour_dict[key]['contour'] = cnt
+      self.contour_dict[key]['area'] = cv2.contourArea(cnt)
+
+    
+    self.wContourList.addItem(self.contour_dict[key]['list_item'])
+    
+    self.wContourList.itemSelectionChanged.connect(self.update_image)
+    self.wContourList.itemClicked.connect(self.update_image)
+    if len(self.contour_dict.keys())>0:
+      self.wContourList.setCurrentItem(self.contour_dict['0 Contour']['list_item'])
+    
+    self.wLayout.addWidget(self.wContourList,0,0)
+
+    self.update_image()
+
+  def update_image(self):
+    self.img_out = np.ones_like(self.mod_in.image())*255
+    selection = self.wContourList.selectedItems()
+    if len(selection) == 1:
+      cnt_key = selection[0].text()
+      cv2.drawContours(self.img_out,self.contours,self.contour_dict[cnt_key]['index'],thickness=4,color=(0,255,0))
+    self.img_item.setImage(self.img_out,levels=(0,255))
+
+  def widget(self):
+    return self.wLayout
+
+  def name(self):
+    return 'Find Contours'
 
 app = QtGui.QApplication([])      
 img_analyzer = ImageAnalyzer()
